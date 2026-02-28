31805bced8d61d5e13c9e22e8d8fd2147bffd3e5
abdulrahman <abdulrahman102034@gmail.com>
2026-02-28 14:28:42 +0200

    dev env setup

---
+++ app.js
@@ -0,0 +1,24 @@
+import express from "express";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+import rateLimit from "express-rate-limit";
+import authRoutes from "./routes/auth.routes.js";
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+app.use(helmet());
+app.use(morgan("dev"));
+
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+});
+
+app.use(limiter);
+
+app.use("/api/auth", authRoutes);
+
+export default app;
